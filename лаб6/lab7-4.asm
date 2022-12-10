339d76d1c40b22c2afb23d0513d0b30380101f25
AnastasiaKoshina <114668065+AnastasiaKoshina@users.noreply.github.com>
2022-12-10 06:27:30 +0300

    Add files via upload

---
+++ лаб6/lab7-4.asm
@@ -0,0 +1,32 @@
+%include    'in_out.asm'
+
+  SECTION .data
+
+  div:  DB 'Результат: ',0
+  rem:  DB ' Остаток от деления: ',0
+
+  SECTION .text
+  GLOBAL _start
+   _start:
+
+   mov eax,12
+   mov ebx,1
+   mul ebx
+   add eax,3
+   xor edx,edx
+   mov ebx,5
+   mul ebx
+
+   mov edi,eax
+
+   mov  eax,div
+   call sprint
+   mov  eax,edi
+   call iprintLF
+
+   mov  eax,rem
+   call sprint
+   mov  eax,edx
+   call iprintLF
+
+   call quit
